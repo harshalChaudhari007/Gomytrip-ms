8bd5f1211fa33a37749dc6314456d6dcaf5ef729
hchaudhari007 <hchaudhari007@gmail.com>
2022-11-10 23:47:11 +0530

    codeRefactor

---
+++ src/test/java/com/example/Gomytrip/GomytripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Gomytrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GomytripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
